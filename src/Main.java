e2c727144eaf1fcc57e777cd72f23e7347978415
Foudre <pierre.goffic@gmail.com>
2017-05-06 17:24:39 +0200

    Joueur et images

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import edu.princeton.cs.introcs.StdDraw;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub$
+		//A gretting
+		//System.out.println("Test");
+		StdDraw.setCanvasSize(1000,1000);
+		StdDraw.setXscale(0,1000);
+		StdDraw.setYscale(0,1000);
+		StdDraw.picture(100, 100, "images/bomberman.png");
+		StdDraw.show();
+	}
+
+}
