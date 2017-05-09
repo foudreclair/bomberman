eb8b384d9b47fb3ebc35b76973b7d7071daaefa5
Foudre <pierre.goffic@gmail.com>
2017-05-09 11:33:38 +0200

    Joueur Start

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,9 @@
-import edu.princeton.cs.introcs.StdDraw;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub$
-		//A gretting
-		//System.out.println("Test");
-		/*StdDraw.setCanvasSize(1000,1000);
-		StdDraw.setXscale(0,1000);
-		StdDraw.setYscale(0,1000);
-//		StdDraw.picture(100, 100, "images/bomberman.png");
-		StdDraw.show();*/
-		//StdDraw.setCanvasSize(1000,1000);
-		//StdDraw.setXscale(0,1000);
-		//StdDraw.setYscale(0,1000);
-		//StdDraw.picture(100, 100, "images/bomberman.png");
-		//StdDraw.show();
-		
-		Map Map=new Map();
-		
+		// TODO Auto-generated method stub
+		Map map = new Map();
 	}
 
 }
