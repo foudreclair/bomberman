4f9f2620a7227076b10a868a17e749fceae59243
Amauzo <Amauzo@users.noreply.github.com>
2017-05-12 23:39:44 +0200

    En mode BG
    Pierre ET JB  GL&HF

---
+++ src/bomberman/Ig.java
@@ -0,0 +1,189 @@
+package bomberman;
+import java.awt.Font;
+import edu.princeton.cs.introcs.*;
+public class Ig {
+
+	//Déclaration de la taille de la map
+		
+	//N est un nombre arbitrairement choisie pour faire une fenêtre de la bonne dimension
+	public int N=50;
+	//l est la longeur
+	public int l=21;
+	//L est la largeur
+	public int L=17;
+	
+	Joueur J1 =new Joueur("J1", 1,1);
+	Joueur J2 =new Joueur("J2", 19,15);
+	
+	public void intialisation_ecran(){
+		
+		StdDraw.setCanvasSize(l*N,L*N);
+		//On défini le nombre de case en graduant notre fenêtre
+		StdDraw.setXscale(0,l);
+		StdDraw.setYscale(0,L);
+		StdDraw.clear(StdDraw.GRAY);	
+		
+	}
+		
+	public void ecran_de_demarrage(){
+		
+		Font font_title= new Font("Arial",Font.BOLD,50);
+		Font font =new Font("Arial",Font.CENTER_BASELINE,30);
+		StdDraw.enableDoubleBuffering();
+		int exit=0;
+		int control=0;
+		while(true){
+			
+			StdDraw.clear(StdDraw.GRAY);
+			StdDraw.setPenColor();
+			StdDraw.setFont(font_title);
+			StdDraw.text(11, 15, "BomberMan");
+			StdDraw.setFont(font);
+			StdDraw.text(11, 4, "New Game");
+			StdDraw.text(11, 2, "Control");
+			StdDraw.picture(11, 10, "images/bomberman.png");
+			StdDraw.pause(80);
+			//Creation des boutons sur la page d'accueil
+			
+			if(StdDraw.mouseX()>=10&& StdDraw.mouseX()<=12&&StdDraw.mouseY()>3&&StdDraw.mouseY()<5){
+				if(StdDraw.mousePressed()){
+					//Test
+					//ecran_fin();
+					break;
+				}
+			}
+			/*tout tourne dans cette boucle 
+			 * 
+			 * if(exit==1){
+				break;
+			}*/
+			//Permet d'afficher les controles des deux joueurs
+			if(StdDraw.mouseX()>=10&& StdDraw.mouseX()<=12&&StdDraw.mouseY()>1&&StdDraw.mouseY()<4){
+				if(StdDraw.mousePressed()){
+					control=1;
+					
+				}
+			}
+			while(control==1){
+				StdDraw.clear(StdDraw.GRAY);
+				StdDraw.text(3, 13, "Joueur 1");
+				StdDraw.text(3, 11, "Z : Move Up");
+				StdDraw.text(3, 10, "Q : Move Left");
+				StdDraw.text(3, 9, "S : Move Down");
+				StdDraw.text(3, 8, "D : Move Right");
+				StdDraw.text(3,  6, "F : Use Bombe");
+				StdDraw.picture(3, 3, "images/bomberman_player.png");
+				StdDraw.text(17, 13, "Joueur 2");
+				StdDraw.text(17, 11, "▲ : Move Up");
+				StdDraw.text(17, 10, "◄ : Move Left");
+				StdDraw.text(17, 9, "▼: Move Down");
+				StdDraw.text(17, 8, "► : Move Right");
+				StdDraw.text(17, 6, "0 : Use Bombe");
+				StdDraw.picture(17, 3, "images/bomferman_player.png");
+				StdDraw.text(11, 2, "Return");
+				
+				StdDraw.pause(80);
+				if(StdDraw.mouseX()>=10&& StdDraw.mouseX()<=12&&StdDraw.mouseY()>1&&StdDraw.mouseY()<3){
+					if(StdDraw.mousePressed()){
+		
+						control=0;
+					}
+				}
+				
+				
+			}
+			
+			StdDraw.show();
+			//StdDraw.pause(100);
+			}
+	}
+		
+	public void ecran_de_jeu(int[][] map){
+		J1.setVie(3);
+		J2.setVie(3);
+		while(J1.getVie()!=0&J2.getVie()!=0){
+		//Colorisation de la map
+		StdDraw.enableDoubleBuffering();
+		StdDraw.clear(StdDraw.GRAY);
+		for(int y=0; y<L; y++){
+			for(int x=0; x<l; x++){
+				if(map[y][x] == 0){
+					StdDraw.setPenColor(StdDraw.GRAY);
+					StdDraw.filledSquare(x+0.5, y+0.5, 0.5);
+				}
+				else if(map[y][x]==1){
+					StdDraw.setPenColor(StdDraw.ORANGE);
+					StdDraw.filledSquare(x+0.5, y+0.5, 0.5);
+				}
+				else if(map[y][x]==2){
+					StdDraw.setPenColor(StdDraw.GREEN);
+					StdDraw.filledSquare(x+0.5, y+0.5, 0.5);
+				}	
+			}
+		}
+		
+		//keycodes d = 68, q=81, z=90, s=83
+		if(StdDraw.isKeyPressed(68)){
+			J1.moveRight(J1, map);
+		}
+		if(StdDraw.isKeyPressed(81)){
+			J1.moveLeft(J1, map);
+		}
+		if(StdDraw.isKeyPressed(90)){
+			J1.moveUp(J1, map);
+		}
+		if(StdDraw.isKeyPressed(83)){
+			J1.moveDown(J1, map);
+		}
+		//keycodes  J2
+		if(StdDraw.isKeyPressed(39)){
+			J2.moveRight(J2, map);
+		}
+		if(StdDraw.isKeyPressed(37)){
+			J2.moveLeft(J2, map);
+		}
+		if(StdDraw.isKeyPressed(38)){
+			J2.moveUp(J2, map);
+		}
+		if(StdDraw.isKeyPressed(40)){
+			J2.moveDown(J2, map);
+		}
+		if(StdDraw.isKeyPressed(32)){
+			J2.setVie(J2.getVie()-1);
+		}
+		StdDraw.pause(40);
+		StdDraw.picture(J1.getX()+0.5,J1.getY()+0.5 , "images/bomberman_player.png");
+		StdDraw.picture(J2.getX()+0.5,J2.getY()+0.5 , "images/bomferman_player.png");
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.text(2+0.5, 16+0.5, "Joueur 1 : "+J1.getVie());
+		StdDraw.show();
+		}
+		
+	}
+	
+	public void ecran_fin(){
+		while(true){
+			StdDraw.enableDoubleBuffering();
+			StdDraw.clear(StdDraw.GRAY);
+			if(J1.getVie()==0){
+				StdDraw.text(11, 13, "Le joueur "+J2.getNom()+" a gagné !");
+			}
+			if(J2.getVie()==0){
+				StdDraw.text(11, 13, "Le joueur "+J1.getNom()+" a gagné !");
+			}
+			StdDraw.picture(11, 9, "images/victory.png");
+			StdDraw.text(11, 4, "Replay");
+			StdDraw.text(11, 2, "Leave");
+			StdDraw.pause(80);
+			if(StdDraw.mouseX()>=9&& StdDraw.mouseX()<=11&&StdDraw.mouseY()>3&&StdDraw.mouseY()<5){
+				if(StdDraw.mousePressed()){
+					ecran_de_demarrage();
+				}
+			}
+		
+			StdDraw.show();
+		
+		}
+	}
+	
+}
---
+++ src/bomberman/Joueur.java
@@ -0,0 +1,101 @@
+package bomberman;
+
+import edu.princeton.cs.introcs.*;
+
+public class Joueur{
+	
+	private String nom;
+	private int vie;
+	private int nbbombes;
+	private int typebombes;
+	private int longeurflame;
+	private int x;
+	private int y;
+	
+	
+	public Joueur(String nom,int x,int y){
+		this.nom = nom;
+		this.x = x;
+		this.y = y;
+		this.vie =3;
+		this.nbbombes = 3;
+		this.typebombes = 1;
+		this.longeurflame =2;
+	
+	}
+	
+	//Déplacement
+	public void moveRight(Joueur j, int[][] map){
+		if(map[y][x+1]==2){
+			j.x+=1;
+			System.out.println("Le joueur " +j.nom+" se déplace a droite.");
+		}else{
+			System.out.println("Mouvement impossible.");
+		}
+	}
+	public void moveLeft(Joueur j, int[][] map){
+		if(map[y][x-1] ==2){
+			j.x-=1;
+			System.out.println("Le joueur " +j.nom+" se déplace a gauche.");
+		}else{
+			System.out.println("Mouvement impossible.");
+		}
+	}
+	public void moveUp(Joueur j, int[][] map){
+		if(map[y+1][x]==2){
+			j.y+=1;
+			System.out.println("Le joueur " +j.nom+" se déplace vers le haut.");
+		}else{
+			System.out.println("Mouvement impossible.");
+		}
+	}
+	public void moveDown(Joueur j, int[][] map){
+		if(map[y-1][x] ==2){
+			j.y-=1;
+			System.out.println("Le joueur " +j.nom+" se déplace vers le bas.");
+		}else{
+			System.out.println("Mouvement impossible.");
+		}
+	}
+	
+	
+	//Getters&Setters
+	public String getNom(){
+		return nom;
+	}
+	//Pour moi elle est inutile
+	public void setNom(String nom){
+		this.nom =nom;
+	}
+	public int getVie(){
+		return vie;
+	}
+	public void setVie(int a){
+		this.vie= a;
+	}
+	public int getNbbombes(){
+		return nbbombes;
+	}
+	public void setNbbombes(int nbbombes){
+		this.nbbombes=nbbombes;
+	}
+	public int getTypebombes(){
+		return typebombes;
+	}
+	public void setTypebombes(int typebombes){
+		this.typebombes= typebombes;
+	}
+	public int getX(){
+		return x;
+	}
+	public void setX(int a){
+		this.x=a;
+	}
+	public int getY(){
+		return y;
+	}
+	public void setY(int a){
+		this.y=a;
+	}
+	
+}
