848a3efd2ccd016a70cb797df00d7b51b4cc474d
Foudre <pierre.goffic@gmail.com>
2017-05-26 09:34:41 +0200

    Bomberman

---
+++ src/bomberman/Main.java
@@ -0,0 +1,36 @@
+package bomberman;
+
+
+import edu.princeton.cs.introcs.StdAudio;
+
+public class Main {
+	
+	public static void main(String[] args){
+		/* Gal�re!!!!
+		 * StdAudio.play("title-screen.wav");
+		 */
+		Map map = new Map();
+		Ig bomberman =new Ig();
+		Joueur J1 = new Joueur("P1", 1, 1);
+		bomberman.intialisation_ecran();
+		bomberman.ecran_de_demarrage();
+		bomberman.ecran_de_jeu(map);
+		bomberman.ecran_fin(map);
+	}
+
+}
+
+
+/*public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Map map = new Map();
+		Joueur j1 = new Joueur("P1", 1.5f, 1.5f);
+		j1.start();
+		j1.moveUp(j1);
+		j1.moveUp(j1);
+		j1.moveRight(j1);
+	}
+
+}*/
